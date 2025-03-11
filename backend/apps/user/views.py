70ccd98d22543c4261126135cf83c7263df25d78
Yar <yamy007>
2025-03-11 20:00:16 +0200

    add user

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
+from .serializers import UserSerializer, UserModel
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+
+class CreateUserAPiView(CreateAPIView):
+    serializer_class = UserSerializer
+
+class GetUserApiView(ListAPIView):
+    permission_classes= (IsAdminUser,)
+    serializer_class = UserSerializer
+    queryset = UserModel.objects.all()
+
+class RetrieveUpdateDestroyUserApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, )
+    
+    def get_object(self):
+        return self.request.user
+    
+class GetMeApiView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,7 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from config.settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
+    path('/users', include('apps.user.urls')),
 ]
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
